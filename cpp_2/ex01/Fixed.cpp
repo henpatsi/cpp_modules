204a70b35571241ff10e9d268fc306128c8897b5
Henri Patsi <hpatsi@student.hive.fi>
2024-04-03 13:38:32 +0300

    working cpp2ex01

--- cpp_2/ex01/Fixed.cpp
+++ cpp_2/ex01/Fixed.cpp
@@ -11,13 +11,15 @@ Fixed::Fixed(void)
 Fixed::Fixed( const int initial_value )
 {
 	std::cout << "Int constructor called\n";
-	this->fixed_point_value = initial_value;
+	this->fixed_point_value = initial_value << this->fractional_bits;
 }
 
 Fixed::Fixed( const float initial_value )
 {
 	std::cout << "Float constructor called\n";
-	this->fixed_point_value = initial_value;
+
+	float rounded = roundf(initial_value * (1 << this->fractional_bits));
+	this->fixed_point_value = (int) rounded;
 }
 
 // COPY
@@ -47,7 +49,13 @@ Fixed& Fixed::operator=(const Fixed& from)
 
 std::ostream& operator<<(std::ostream& out, const Fixed& from)
 {
-	out << from.getRawBits();
+	int	integral_bits = (sizeof(int) * 8) - from.fractional_bits;
+	int	decimal = from.fixed_point_value << integral_bits;
+
+	if (decimal > 0)
+		out << from.toFloat();
+	else
+		out << from.toInt();
 	return out;
 }
 
@@ -65,10 +73,13 @@ void	Fixed::setRawBits ( int const raw )
 
 int	Fixed::toInt( void ) const
 {
-	return this->fixed_point_value;
+	int	int_value;
+
+	int_value = this->fixed_point_value >> this->fractional_bits;
+	return int_value;
 }
 
 float	Fixed::toFloat( void ) const
 {
-	return this->fixed_point_value;
+	return ((float) this->fixed_point_value / (float) (1 << this->fractional_bits));
 }
