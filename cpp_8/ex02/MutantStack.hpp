041b02867adb3fbacc95c1f58c0e67487063e9fe
Henri Pätsi <patsihenri@gmail.com>
2024-09-02 15:30:31 +0300

    cpp8ex2 working

--- cpp_8/ex02/MutantStack.hpp
+++ cpp_8/ex02/MutantStack.hpp
@@ -6,14 +6,19 @@
 /*   By: hpatsi <hpatsi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/30 14:50:53 by hpatsi            #+#    #+#             */
-/*   Updated: 2024/08/30 14:57:49 by hpatsi           ###   ########.fr       */
+/*   Updated: 2024/09/02 15:25:44 by hpatsi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stack>
+#include <deque>
 
-template<class T, class Container = std::stack<T>>
-class MutantStack
+template<typename T>
+class MutantStack : public std::stack<T>
 {
-    
+	public:
+		typedef typename std::deque<T>::iterator iterator;
+
+		iterator begin() { return this->c.begin(); }
+		iterator end() { return this->c.end(); }
 };
--- cpp_8/ex02/main.cpp
+++ cpp_8/ex02/main.cpp
@@ -6,36 +6,65 @@
 /*   By: hpatsi <hpatsi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/29 18:39:47 by hpatsi            #+#    #+#             */
-/*   Updated: 2024/08/30 14:57:19 by hpatsi           ###   ########.fr       */
+/*   Updated: 2024/09/02 15:29:55 by hpatsi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <list>
 
 #include "MutantStack.hpp"
 
 int main()
 {
 	MutantStack<int> mstack;
+	std::list<int> list;
+
 	mstack.push(5);
+	list.push_back(5);
 	mstack.push(17);
+	list.push_back(17);
+
 	std::cout << mstack.top() << std::endl;
+	std::cout << list.back() << std::endl;
+
 	mstack.pop();
+	list.pop_back();
+
 	std::cout << mstack.size() << std::endl;
+	std::cout << list.size() << std::endl;
+
 	mstack.push(3);
+	list.push_back(3);
 	mstack.push(5);
+	list.push_back(5);
 	mstack.push(737);
-	//[...]
+	list.push_back(737);
 	mstack.push(0);
+	list.push_back(0);
+
 	MutantStack<int>::iterator it = mstack.begin();
 	MutantStack<int>::iterator ite = mstack.end();
 	++it;
 	--it;
+	std::cout << "Printing MutantStack:\n";
 	while (it != ite)
 	{
-	std::cout << *it << std::endl;
-	++it;
+		std::cout << *it << std::endl;
+		++it;
 	}
+
+	std::list<int>::iterator lit = list.begin();
+	std::list<int>::iterator lite = list.end();
+	++lit;
+	--lit;
+	std::cout << "Printing list:\n";
+	while (lit != lite)
+	{
+		std::cout << *lit << std::endl;
+		++lit;
+	}
+
 	std::stack<int> s(mstack);
 	return 0;
 }
