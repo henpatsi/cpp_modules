3eebd5461f96fe5cfe1de1d2258be0757ed4d57d
Henri Patsi <hpatsi@student.hive.fi>
2024-09-04 11:31:21 +0300

    cpp8ex2 added reverse and const iterators

--- cpp_8/ex02/MutantStack.hpp
+++ cpp_8/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: hpatsi <hpatsi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/30 14:50:53 by hpatsi            #+#    #+#             */
-/*   Updated: 2024/09/02 16:25:13 by hpatsi           ###   ########.fr       */
+/*   Updated: 2024/09/04 11:29:41 by hpatsi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,27 @@
 #include <stack>
 #include <deque>
 
-template<typename T>
-class MutantStack : public std::stack<T>
+template<typename T, typename Container = std::deque<T>>
+class MutantStack : public std::stack<T, Container>
 {
 	public:
 		MutantStack() {};
-		MutantStack(const MutantStack& other) : std::stack<T>(other) {};
+		MutantStack(const MutantStack& other) : std::stack<T, Container>(other) {};
 		~MutantStack() {};
 		MutantStack& operator=(const MutantStack& other) { this->c = other.c; return *this; }
 
-		typedef typename std::deque<T>::iterator iterator;
+		typedef typename Container::iterator iterator;
+		typedef typename Container::reverse_iterator reverse_iterator;
+		typedef typename Container::const_iterator const_iterator;
+		typedef typename Container::const_reverse_iterator const_reverse_iterator;
 
 		iterator begin() { return this->c.begin(); }
 		iterator end() { return this->c.end(); }
+		const_iterator begin() const { return this->c.begin(); }
+		const_iterator end() const { return this->c.end(); }
+
+		reverse_iterator rbegin() { return this->c.rbegin(); }
+		reverse_iterator rend() { return this->c.rend(); }
+		const_reverse_iterator rbegin() const { return this->c.rbegin(); }
+		const_reverse_iterator rend() const { return this->c.rend(); }
 };
